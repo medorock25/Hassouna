ac0e8bb338457215fb7c1dc4f419fb244fcadf45
Mohamed Hassouna <mohamed@oc2616644641.ibm.com>
2019-11-03 00:55:12 +0100

    first stage

--- scratch_5.py
+++ scratch_5.py
@@ -1,6 +1,11 @@
-cool = [1 , 2 , 3, 4, 5]
+cool = {'floor': [0,1,2,3,4,5]}
+position = cool.values()
+target = int(input("Enter a number: "))
 
-print(type(cool))
 
-print(range(1,3))
-for i in range(1,3)
+for position in cool.values():
+    while position != target:
+        print("going to {}".format(target))
+        if position == target:
+            print("stay")
+
